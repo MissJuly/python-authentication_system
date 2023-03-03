031c5b068045d6cdf7adef0a214926e91ed07a6a
Barissa-Imran <imranbarissa@gmail.com>
2023-03-04 00:07:25 +0300

    Add sign in function
    Split code into functions

--- __main__.py
+++ __main__.py
@@ -1,28 +1,81 @@
+"""
+New User:
+(Registration)
+- Print welcome message
+- Take user input: username & Password - confirm password
+- Ask user to login with given credentials
+- login the user
+- print logged in message
+
+(Sign In)
+- enter username and password
+- Validate the data given
+- log in user or retry with credentials.
+
+(Update)
+- update either username or password *change
+"""
+import re
+
 print("Welcome to our app!")
+print()
+
+def to_json(username, password):
+    pass
+
+def update(reg_username, reg_password):
+    pass
+
+
+def sign_in(reg_username=None, reg_password=None):
+    """sign in the user into the system"""
+
+    while True:
+        print()
+        print("Please log in!")
+        print()
+
+        login_username = str(input("Enter username: "))
+        login_password = input("Enter password: ")
+
+        if login_username == reg_username and login_password == reg_password:
+            print()
+            print(f"You have successfully logged in as {login_username}")
+            break
+        else:
+            print("Username or password incorrect. Try again!")
+            continue
+
 
 def registration():
     """Signs up new user"""
-    username = str(input("Enter username: "))
+    print("Please create an account.")
+    print()
+
+    reg_username = str(input("Enter username: "))
 
     while True:
-        password1 = input("Enter password:(Must contain at least 8 characters, Uppercase, lowercase, numbers and special characters)")
-        password2 = input("Confirm password: ")
+        print()
+        reg_password1 = input("Enter password - (Must contain at least 8 characters, Uppercase, lowercase, numbers and special characters): ")
+        # password validation
+        regex_pattern = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+        if re.match(regex_pattern, reg_password1):
+            while True:
+                reg_password2 = input("Confirm password: ")
 
-        if password1 == password2:
-            print("Please log in!")
-
-            username1 = str(input("Enter username: "))
-            password = input("Enter password: ")
-
-            if username == username1 and password == password1:
-                print("You have sucessfully logged in!")
-                break
-            else:
-                print("Username or password incorrect. Try again!")
-
+                if reg_password1 == reg_password2:
+                    print("You have successfully signed up!")
+                    sign_in(reg_username=reg_username, reg_password=reg_password1)
+                    break
+                else:
+                    print()
+                    print("Passwords do not match!")
+                    continue
+            break
         else:
+            print("Password Must contain at least 8 characters, Uppercase, lowercase, numbers and special characters")
             continue
 
+
+
 registration()
-
-
